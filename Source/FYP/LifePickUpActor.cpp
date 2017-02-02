f79cbdb5b7e296182950c2de83f0797c184d6367
Taikatou <13123947@studentmail.ul.ie>
2017-02-02 13:00:21 +0000

    lifepickup

---
+++ Source/FYP/LifePickUpActor.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FYP.h"
+#include "LifePickUpActor.h"
+
+// Set default values
+ALifePickUpActor::ALifePickUpActor()
+{
+	GetMesh()->SetSimulatePhysics(true);
+}
+
+
---
+++ Source/FYP/LifePickUpActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PickUpActor.h"
+#include "LifePickUpActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FYP_API ALifePickUpActor : public APickUpActor
+{
+	GENERATED_BODY()
+	
+public:
+	// Set default values
+	ALifePickUpActor();
+	
+};
