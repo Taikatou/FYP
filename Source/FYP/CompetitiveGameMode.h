bae30e38bbae8a312f307e58c24634adc0bc9bd5
Taikatou <13123947@studentmail.ul.ie>
2017-04-03 11:34:56 +0100

    changes before aws

---
+++ Source/FYP/CompetitiveGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CompetitiveGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FYP_API ACompetitiveGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	ACompetitiveGameMode();
+	
+	
+};
